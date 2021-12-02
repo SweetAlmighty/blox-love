1468722a357f9859b5abdc7d8038d59b4eee6c32
Brian Sweet <bsweet48@gmail.com>
2021-12-01 20:57:26 -0500

    issue/59
    Finished second pass of background.

--- main.lua
+++ main.lua
@@ -7,27 +7,17 @@ local StateMachine = require "src/states/statemachine"
 
 state_machine = StateMachine()
 
-function love.draw() state_machine:draw() end
-
-function love.resize() state_machine:resize() end
-
-function love.update(dt) state_machine:update(dt) end
-
-function love.keypressed(key) state_machine:input(key) end
-
-function love.mousemoved(x, y, dx, dy, istouch) state_machine:mouse_moved(x, y, dx, dy, istouch) end
-
-function love.mousepressed(x, y, button, istouch, presses)
-    state_machine:mouse_pressed(x, y, button, istouch, presses)
-end
-
-function love.mousereleased(x, y, button, istouch, presses)
-    state_machine:mouse_released(x, y, button, istouch, presses)
-end
-
 function love.load()
     Utils.seed_rand()
     Resources.load()
     UI.set_start_style()
     state_machine:push(GameStates.MainMenu)
 end
+
+function love.draw() state_machine:draw() end
+function love.resize() state_machine:resize() end
+function love.update(dt) state_machine:update(dt) end
+function love.keypressed(key) state_machine:input(key) end
+function love.mousemoved(x, y, dx, dy, istouch) state_machine:mouse_moved(x, y, dx, dy, istouch) end
+function love.mousepressed(x, y, button, istouch, presses) state_machine:mouse_pressed(x, y, button, istouch, presses) end
+function love.mousereleased(x, y, button, istouch, presses) state_machine:mouse_released(x, y, button, istouch, presses) end
--- src/menubackground.lua
+++ src/menubackground.lua
@@ -1,6 +1,7 @@
 local Color = require "src/utils/color"
 local Utils = require "src/utils/utils"
 local Object = require "src/lib/classic"
+local Updater = require "src/utils/updater"
 local moonshine = require "src/lib/moonshine"
 local Resources = require "src/utils/resources"
 
@@ -9,11 +10,60 @@ local MenuBackground = Object:extend()
 local color_index = 1
 local rnd = math.random
 local lg = love.graphics
-local updatesPerSec = 60
-local nextUpdate = Utils.now()
-local currentTime = Utils.now()
-local skipTicks = 1000/updatesPerSec
 local windowWidth, windowHeight = lg.getWidth(), lg.getHeight()
+
+local function draw_blocks(self)
+    self._sprite_batch:clear()
+    for _, v in ipairs(self._blocks) do
+        for _, w in ipairs(v) do
+            self._sprite_batch:setColor(w.color.r, w.color.g, w.color.b)
+            self._sprite_batch:add(w.x, w.y, self._rotate, 1, 1, self._img_size.hw, self._img_size.hh)
+        end
+    end
+    lg.draw(self._sprite_batch)
+end
+
+local function update_and_clamp_blocks(self)
+    self._delta = 0.01 * self._offset
+    self._rotate = self._rotate + self._delta
+    self._start = math.min(1, math.max(-1, self._start + self._delta))
+
+    for _, v in ipairs(self._blocks) do
+        for _, w in ipairs(v) do
+            if w.x < -self._img_size.w then
+                w.x = self._width
+            elseif w.x > self._width then
+                w.x = -self._img_size.w
+            else
+                w.x = w.x + self._start
+            end
+        end
+    end
+end
+
+local function create_blocks(self)
+    local color = nil
+
+    local w = math.floor((windowWidth / self._img_size.w) / 2) + 2
+    local h = math.floor((windowHeight / self._img_size.h) / 2) + 1
+
+    for i = 1, h do
+        local row = {}
+        for j = 1, w do
+            color = Color.colors[color_index]
+            color_index = (color_index % #Color.colors) + 1
+
+            row[#row + 1] = {
+                color = color,
+                x = (self._img_size.w * (j - 1) * 2),
+                y = (self._img_size.h * (i - 1) * 2) + self._img_size.hh
+            }
+        end
+        self._blocks[#self._blocks + 1] = row
+    end
+
+    self._width = self._blocks[#self._blocks][w].x + self._img_size.w
+end
 
 function MenuBackground:new()
     self._start = 0
@@ -23,61 +73,23 @@ function MenuBackground:new()
     self._blocks = {}
     self._image = Resources.load_image('block')
     self._sprite_batch = lg.newSpriteBatch(self._image)
-    self._imgSize = {
+    self._draw_blocks = function() draw_blocks(self) end
+    self._update_blocks = function() update_and_clamp_blocks(self) end
+    self._img_size = {
         w = self._image:getWidth(), h = self._image:getHeight(),
         hw = self._image:getWidth() / 2, hh = self._image:getHeight() / 2
     }
 
+    self._updater = Updater(60, self._update_blocks)
+
     self._box_blur = moonshine(moonshine.effects.boxblur)
     self._box_blur.boxblur.radius = 5
 
-    local color = nil
-    for i = 1, 25 do
-        color = Color.colors[color_index]
-        color_index = color_index == #Color.colors and 1 or color_index + 1
-        self._blocks[#self._blocks + 1] = {
-            color = color,
-            y = rnd(self._imgSize.hh, windowHeight - self._imgSize.hh),
-            x = rnd(self._imgSize.hw, (windowWidth + self._imgSize.w) - self._imgSize.hw)
-        }
-    end
+    create_blocks(self)
 end
 
-function MenuBackground:draw()
-    self._box_blur(function()
-        self._sprite_batch:clear()
-
-        for i, v in ipairs(self._blocks) do
-            self._sprite_batch:setColor(v.color.r, v.color.g, v.color.b)
-            self._sprite_batch:add(v.x, v.y, self._rotate, 1, 1, self._imgSize.hw, self._imgSize.hh)
-        end
-
-        lg.draw(self._sprite_batch)
-    end)
-end
-
-function MenuBackground:update(dt)
-    currentTime = Utils.now()
-    if nextUpdate < currentTime then
-        self._delta = 0.01 * self._offset
-        self._rotate = self._rotate + self._delta
-        self._start = math.min(1, math.max(-1, self._start + self._delta))
-
-        for i, v in ipairs(self._blocks) do
-            if v.x < -self._imgSize.w then
-                v.x = windowWidth + self._imgSize.w
-                v.y = rnd(self._imgSize.hh, windowHeight - self._imgSize.hh)
-            elseif v.x > windowWidth + self._imgSize.w then
-                v.x = -self._imgSize.w
-                v.y = rnd(self._imgSize.hh, windowHeight - self._imgSize.hh)
-            else
-                v.x = v.x + self._start
-            end
-        end
-        nextUpdate = nextUpdate + skipTicks
-    end
-end
-
+function MenuBackground:update(dt) self._updater:update() end
+function MenuBackground:draw() self._box_blur(self._draw_blocks) end
 function MenuBackground:mouse_moved(x, y, dx, dy, istouch) self._offset = (x - (windowWidth / 2)) / (windowWidth / 2) end
 
 return MenuBackground
